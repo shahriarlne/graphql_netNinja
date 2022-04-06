8307b65c31c4d762f19cd4782447865461887252
shahriarlne <shahriar@thedoodleinc.com>
2022-04-06 14:28:47 +0600

    change schema

--- schema/schema.js
+++ schema/schema.js
@@ -1,10 +1,11 @@
 const graphql = require('graphql');
 
 const {GraphQLObjectType,GraphQLString} = graphql;
-
 const BookType = new GraphQLObjectType({
     name:'Book',
-    fields:() =>{
-        id:{type: GraphQLString}
-    }
+    fields:() =>({
+        id:{type: GraphQLString},
+        name:{type:GraphQLString},
+        genre:{type:GraphQLString}
+    })
 })
